a6c7ca170477c64c6a955e76b689d48982478910
Gulsaba Fiha <sabafiha111@gmail.com>
2024-05-03 20:59:32 +0600

    initialize git for this project

---
+++ webcam_test_turjo.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+
+import cv2
+
+from facetools import FaceDetection, IdentityVerification, LivenessDetection
+from facetools.utils import visualize_results
+
+root = Path(os.path.abspath(__file__)).parent.absolute()
+data_folder = root / "data"
+
+resNet_checkpoint_path = data_folder / "checkpoints" / "InceptionResnetV1_vggface2.onnx"
+facebank_path = data_folder / "jj_v1.csv"
+
+deepPix_checkpoint_path = data_folder / "checkpoints" / "OULU_Protocol_2_model_0_0.onnx"
+
+faceDetector = FaceDetection(max_num_faces=2)
+identityChecker = IdentityVerification(
+    checkpoint_path=resNet_checkpoint_path.as_posix(),
+    facebank_path=facebank_path.as_posix(),
+)
+livenessDetector = LivenessDetection(checkpoint_path=deepPix_checkpoint_path.as_posix())
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    canvas = frame.copy()
+    faces, boxes = faceDetector(frame)
+    for face_arr, box in zip(faces, boxes):
+        min_sim_score, mean_sim_score = identityChecker(face_arr)
+        liveness_score = livenessDetector(face_arr)
+        canvas = visualize_results(canvas, box, liveness_score, mean_sim_score)
+    cv2.imshow("face", canvas)
+    k = cv2.waitKey(1)
+    if k == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
